463f562353345bbf652860a7c40a3c3f2abca530
Deb Johnson <friendswithdrj@gmail.com>
2020-06-16 16:31:14 -0500

    Add logic to function to count letters

--- src/letter-count.js
+++ src/letter-count.js
@@ -1,2 +1,11 @@
 // Example: cat' -> { c: 1, a: 1, t: 1}
-export const getLetterCount = string => ({});
+export const getLetterCount = string => {
+  const letters = string.split('');
+  let letterCount = {};
+  letters.forEach(letter => {
+    if (!letterCount[letter]) {
+      letterCount[letter] = 1;
+    }
+  })
+  return letterCount;
+};
